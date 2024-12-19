53fadd251454a169e192ddb45144f0eecffd7a2e
Maxdeoro <maxdeoro@gmail.com>
2024-12-19 22:53:09 +0300

    code modified

--- FancyBoxV2/js/script.js
+++ FancyBoxV2/js/script.js
@@ -1,3 +1,18 @@
-$(function() {
-	$('a').fancybox();
+$(document).ready(function() {
+	$('[data-fancybox="gallery"]').fancybox({
+		buttons: [
+			"zoom",
+			"slideShow",
+			"thumbs",
+			"close"
+		],
+		loop: true,
+		caption: function(instance, item) {
+			return $(this).find('img').attr('alt');
+		},
+		// transitionEffect: "circular",
+		animationEffect: "fade",
+		animationDuration: 1000,
+	});
 });
+
